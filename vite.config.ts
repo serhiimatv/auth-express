c50fd4ef8bc984a144bb909ae2a3a99e0ca24a2a
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2024-09-22 21:56:57 +0300

    add mui

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import express from "./express-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), express("src/server")],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["module:@preact/signals-react-transform"]],
+      },
+    }),
+    express("src/server"),
+  ],
 });
